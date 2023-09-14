cec7c8982eecf8dd25f932e676db93c9a85391c8
Thanetl <thanet.l@dcs.premier.co.th>
2023-09-14 16:18:52 +0700

    feat : create

--- web/product-app/src/app/product-create/product-create.component.ts
+++ web/product-app/src/app/product-create/product-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-create',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
+  productForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private productService: ProductService) {
+    this.productForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', [Validators.required, Validators.min(0)]],
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    if (this.productForm.valid) {
+      const productData = this.productForm.value;
+      // Send the product data to the service for API communication.
+      this.productService.createProduct(productData).subscribe(
+        (response) => {
+          console.log('Product created:', response);
+        },
+        (error) => {
+          console.error('Error creating product:', error);
+        }
+      );
+    } else {
+      // Handle form validation errors
+    }
+  }
 }
